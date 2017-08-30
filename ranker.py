c9aeec30c3eb5df8101a29a8d4b6380e81c1cc01
lemon <dgodman@gmail.com>
2017-08-30 09:09:06 -0400

    rank paragraphs

--- ranker.py
+++ ranker.py
@@ -50,9 +50,11 @@ class AbstractTokenizer:
         else:
             return _word_list
 
-    def word_frequency(self):
+    def word_frequency(self, _text=None):
         word_count = {}
-        words = self.clean_words(self.text)
+        if not _text:
+            _text = self.text
+        words = self.clean_words(_text)
         if words:
             c = Counter(words).most_common()
             for wc in c:
@@ -61,10 +63,28 @@ class AbstractTokenizer:
                 word_count[s] = i
         return word_count
 
+    def key_words(self, _text=None):
+        if not _text:
+            _text = self.text
+        return list(self.word_frequency(_text))
+
 
 class Rankit(AbstractTokenizer):
     def __init__(self, stem_lang="english"):
         super(Rankit, self).__init__(stem_lang)
+        self.sentence_words = []
+
+    def tokenize(self, _text, token_p=True, token_s=True, token_w=True):
+        super(Rankit, self).tokenize(_text, token_p, token_s, token_w)
+        for sent in self.sentences:
+            words = self.clean_words(sent)
+            self.sentence_words.append(words)
+
+    def get_sentence_words(self, i):
+        words = self.sentence_words[i]
+        if not words:
+            words = self.clean_words(self.sentences[i])
+        return words
 
     @staticmethod
     def rank_edges(_words1, _words2):
@@ -110,3 +130,23 @@ class Rankit(AbstractTokenizer):
             _count = int(len(self.sentences) * 0.25)
         scores = self.score_sentences()
         return sort_by_index(scores)[:_count]
+
+    def rank_paragraphs(self, _count=-1):
+        inters = []
+        key_list = []
+        # get length
+        n = len(self.paragraphs)
+        # create list of keywords for each paragraph
+        for p in self.paragraphs:
+            key_list.append(self.key_words(p))
+        # compare each paragraph with another
+        for i in range(0, n):
+            interval = 0
+            k1 = set(key_list[i])
+            for j in range(0, n):
+                if i != j:
+                    k2 = set(key_list[j])
+                    interval += len(k1.intersection(k2))
+            inters.append(interval)
+        #return sort_by_index(inters)
+        return inters
