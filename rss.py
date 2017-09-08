0a1b2142149c075554bedf5af22a3e1ae027e5b5
lemon <dgodman@gmail.com>
2017-09-08 14:49:16 -0400

    added links

--- rss.py
+++ rss.py
@@ -1,8 +1,22 @@
 import feedparser
 
 
-breitbart_url = 'https://feeds.feedburner.com/breitbart'
-nationalreview_url = 'https://www.nationalreview.com/rss.xml'
+# right
+breitbart_url = 'http://feeds.feedburner.com/breitbart'
+nationalreview_url = 'http://www.nationalreview.com/rss.xml'
+foxnews_url = 'http://feeds.foxnews.com/foxnews/politics'
+watimes_url = 'http://www.washingtontimes.com/rss/headlines/news/politics/'
+drudge_url = 'http://www.drudgereportfeed.com/'
+redstate_url = ''
+# left/moderate
+wapo_url = 'https://feeds.washingtonpost.com/rss/politics'
+npr_url = 'http://www.npr.org/rss/rss.php?id=1014'
+huffington_url = 'http://www.huffingtonpost.com/section/politics/feed'
+nyt_url = 'http://rss.nytimes.com/services/xml/rss/nyt/Politics.xml'
+thehill_url = 'http://thehill.com/rss/syndicator/19110'
+cnn_url = 'http://rss.cnn.com/rss/cnn_allpolitics.rss'
+vox_url = 'http://www.vox.com/rss/index.xml'
+reuters_url = 'http://feeds.reuters.com/Reuters/PoliticsNews'
 
 fp = feedparser.parse(breitbart_url)
 feed_title = fp['feed']['title']  # 'Breitbart News'
